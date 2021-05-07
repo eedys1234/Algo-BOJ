483b683b095fd048d63aee06f8de7feebc963e1d
이정환 <eedys1234@naver.com>
2021-05-07 23:44:54 +0900

    BOJ - 3865

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,3 +5,7 @@
 build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
 N
 src/main/java/kmp/PS_3356.java,9\b\9b4161c58cc02af1256a81fcdae91f3f9aedc3f6
+N
+src/main/java/kmp/PS_1305.java,1\f\1fa3b8a369debdef49b4a5b268525989744eb4af
+]
+-src/main/java/BipartiteMatching/PS_11367.java,4\a\4a675bcd5a99fa3f793dd917fbde2d2bace6e841
---
+++ src/main/java/PS_3865.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class PS_3865 {
+    public static int count = 0;
+
+    public static void main(String... args) throws Exception {
+
+        BufferedReader inbr = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while(true) {
+            int n = Integer.valueOf(inbr.readLine());
+            if(n == 0) {
+                break;
+            }
+            count = 0;
+            String[] temp = new String[n];
+
+            for(int i=0;i<n;i++)
+            {
+                temp[i] = inbr.readLine();
+            }
+
+            Map<String, List<String>> store = new LinkedHashMap<>();
+
+            Map<String, Boolean> visited = Stream.of(temp)
+                    .flatMap(key -> Stream.of(key.split(":")[1].replace(".", "").split(",")))
+                    .collect(Collectors.toSet())
+                    .stream()
+                    .collect(Collectors.toMap(Function.identity(), key -> false));
+
+            for(int i=0;i<temp.length;i++)
+            {
+                String[] value = temp[i].split(":");
+                String academy = value[0];
+                List<String> list = Stream.of(value[1].replace(".", "").split(","))
+                                            .collect(Collectors.toList());
+                store.put(academy, list);
+            }
+
+            dfs(store, visited, temp[0].split(":")[0]);
+            sb.append(count).append("\r\n");
+
+        }
+
+        System.out.println(sb.toString());
+
+    }
+
+
+    public static void dfs(Map<String, List<String>> store, Map<String, Boolean> visited, String start) {
+
+        List<String> list = store.get(start);
+
+        for(String str : list)
+        {
+            if(!visited.get(str)) {
+                visited.put(str, true);
+                if(store.containsKey(str)) {
+                    dfs(store, visited, str);
+                }
+                else {
+                    count++;
+                }
+            }
+        }
+    }
+}
